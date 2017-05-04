2df71f46cd1e9dedbbf69f506f959061ebfe0447
Dion Hagan <dionhagan@college.harvard.edu>
2017-05-04 12:53:22 -0400

    added structor, material UI, and react-devtools

---
+++ .structor/app/components.js
@@ -0,0 +1,14 @@
+import {
+    Link,
+    IndexLink
+} from 'react-router';
+import * as MUI from 'modules/MUI';
+import * as MUIIcons from 'modules/MUIIcons';
+import * as Utilities from 'modules/Utilities';
+export {
+    Link,
+    IndexLink,
+    MUI,
+    MUIIcons,
+    Utilities
+};
---
+++ src/modules/MUI/index.js
@@ -0,0 +1,11 @@
+require('./css/styles.css');
+import ThemeProvider from './components/ThemeProvider';
+import LayoutContainer from './components/LayoutContainer';
+import LayoutItem from './components/LayoutItem';
+import Button from 'material-ui/Button';
+export {
+  ThemeProvider,
+  LayoutContainer,
+  LayoutItem,
+  Button
+};
